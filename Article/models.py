02c762e58cb0be1c09a44b3f2a5c7fcf07b2b7fd
3A4EM4EMPION <anton.3a4em@gmail.com>
2016-09-08 19:14:12 +0300

    create new application Article

--- Article/models.py
+++ Article/models.py
@@ -3,3 +3,24 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+	"""Article discription"""
+
+	class Meta(object):
+		db_table = "Article"
+			
+	article_text = models.TextField()
+	article_title = models.CharField(max_length=255)
+	article_date = models.DateTimeField()
+	article_likes = models.IntegerField(default=0)
+
+class Comment(models.Model):
+	"""Comments discription"""
+	class Meta(object):
+		db_table = "Comment"
+
+	comment_text = models.TextField()
+	comment_date = models.DateTimeField()
+	comment_article = models.ForeignKey(Article)
+
+		
